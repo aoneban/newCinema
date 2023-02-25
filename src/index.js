eec8ea5bd3001e88811501178f16c639b57b2555
aoneban <torgregion1@gmail.com>
2023-02-25 20:16:04 +0100

    feat: added footer

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import './index.html';
 import './index.scss';
 import { API_URL } from './modules/api';
 import { createHeader } from './pages/Header';
-import { generateMovie } from './modules/utils';
+import { generateMovie, getFavoritMovie, getWishMovie, paginationMovies } from './modules/utils';
+import { createFooter } from './pages/Footer';
 
 createHeader();
-generateMovie(API_URL);
+generateMovie(API_URL, 1, getFavoritMovie(), getWishMovie());
+setTimeout(paginationMovies, 500)
+createFooter();
--- src/modules/utils.js
+++ src/modules/utils.js
@@ -1,51 +1,111 @@
-import { getMovie } from "./api";
+import { getMovie, API_URL } from './api';
+import { createFooter } from '../pages/Footer';
 
-export const generateMovie = async (url) => {
-    const data = await getMovie(url);
-    const root = document.getElementById('root');
-    const contentWrap = document.createElement('div');
-    contentWrap.classList.add('content-wrapper');
-    const favoriteMovie = document.createElement('div');
-    favoriteMovie.classList.add('favorite-movie');
-    const wishMovie = document.createElement('div');
-    wishMovie.classList.add('wish-movie');
-  
-    data.films.forEach((elem) => {
-      const listMovies = document.createElement('div');
-      listMovies.classList.add('list-movies2');
-  
-      const movieCart = document.createElement('div');
-      movieCart.classList.add('movie__cart');
-  
-      const movieCover = document.createElement('div');
-      movieCover.classList.add('movie__cover-inner');
-  
-      const img = document.createElement('img');
-      img.classList.add('poster-img');
-      img.src = elem.posterUrlPreview;
-      img.alt = elem.nameRu;
-  
-      movieCover.append(img);
-  
-      const movieInfo = document.createElement('div');
-      movieInfo.classList.add('movie__info');
-  
-      const movieTitle = document.createElement('div');
-      movieTitle.classList.add('movie__title');
-      movieTitle.textContent = elem.nameRu;
-  
-      const movieCategory = document.createElement('div');
-      movieCategory.classList.add('movie__category');
-      movieCategory.textContent = elem.genres.map((el) =>'\n' + el.genre);
-  
-      const movieAverage = document.createElement('div');
-      movieAverage.classList.add('movie__average');
-      movieAverage.textContent = elem.rating;
-  
-      movieInfo.append(movieTitle, movieCategory, movieAverage);
-      movieCart.append(movieCover, movieInfo);
-      listMovies.append(movieCart);
-      contentWrap.append(listMovies, favoriteMovie, wishMovie);
-      root.append(contentWrap);
-    });
-  };
+export const getFavoritMovie = () => {
+  const favoriteMovie = document.createElement('section');
+  favoriteMovie.classList.add('favorite-movie');
+
+  const img = document.createElement('img');
+  img.src = 'https://picsum.photos/910/400';
+  favoriteMovie.append(img);
+  favoriteMovie.style.overflow = 'hidden';
+  return favoriteMovie;
+};
+
+export const getWishMovie = () => {
+  const wishMovie = document.createElement('section');
+  wishMovie.classList.add('wish-movie');
+  return wishMovie;
+};
+
+export const generateMovie = async (url, page, f1, f2) => {
+  const data = await getMovie(url, page);
+  const root = document.getElementById('root');
+  const contentWrap = document.createElement('main');
+  contentWrap.classList.add('content-wrapper');
+
+  data.films.forEach((elem) => {
+    const listMovies = document.createElement('div');
+    listMovies.classList.add('list-movies');
+
+    const movieCart = document.createElement('div');
+    movieCart.classList.add('movie__cart');
+
+    const movieCover = document.createElement('div');
+    movieCover.classList.add('movie__cover-inner');
+
+    const img = document.createElement('img');
+    img.classList.add('poster-img');
+    img.src = elem.posterUrlPreview;
+    img.alt = elem.nameRu;
+
+    movieCover.append(img);
+
+    const movieInfo = document.createElement('div');
+    movieInfo.classList.add('movie__info');
+
+    const movieTitle = document.createElement('div');
+    movieTitle.classList.add('movie__title');
+    movieTitle.textContent = elem.nameRu;
+
+    const movieCategory = document.createElement('div');
+    movieCategory.classList.add('movie__category');
+    movieCategory.textContent = elem.genres.map((el) => '\n' + el.genre);
+
+    const movieAverage = document.createElement('div');
+    movieAverage.classList.add('movie__average');
+    movieAverage.textContent = elem.rating;
+
+    movieInfo.append(movieTitle, movieCategory, movieAverage);
+    movieCart.append(movieCover, movieInfo);
+    listMovies.append(movieCart);
+    contentWrap.append(listMovies, f1, f2);
+    root.append(contentWrap);
+  });
+};
+
+// current page
+let NUM_PAGE = 1;
+
+export const paginationMovies = () => {
+  const root = document.getElementById('root');
+  const buttonWrapper = document.createElement('div');
+  buttonWrapper.classList.add('button-wrapper');
+
+  const footerWrapper = document.querySelector('.footer-wrapper')
+
+  const numberOfPage = document.createElement('p');
+  numberOfPage.textContent = `Page number: ${NUM_PAGE}`;
+
+  const buttonTwo = document.createElement('button');
+  const movies = document.querySelector('.content-wrapper');
+
+  const buttonOne = document.createElement('button');
+  buttonOne.textContent = '< prev';
+  buttonOne.addEventListener('click', () => {
+    NUM_PAGE--;
+    if (NUM_PAGE < 1) {
+      NUM_PAGE = 1;
+    }
+    footerWrapper.remove();
+    buttonWrapper.remove();
+    movies.remove();
+    generateMovie(API_URL, NUM_PAGE, getFavoritMovie(), getWishMovie());
+    setTimeout(paginationMovies, 500);
+    createFooter();
+  })
+
+  buttonTwo.textContent = 'next >';
+  buttonTwo.addEventListener('click', () => {
+    NUM_PAGE++;
+    footerWrapper.remove();
+    buttonWrapper.remove();
+    movies.remove();
+    generateMovie(API_URL, NUM_PAGE, getFavoritMovie(), getWishMovie());
+    setTimeout(paginationMovies, 500);
+    createFooter();
+  });
+
+  buttonWrapper.append(buttonOne, numberOfPage, buttonTwo);
+  root.append(buttonWrapper);
+};
